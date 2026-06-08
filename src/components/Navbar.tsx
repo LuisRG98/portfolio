cd4efb0140eb6aca2ff39d7d9bf9beb39bd3aa8e
Luis Rivas <luisricardorivasgiwencer@gmail.com>
2026-06-08 15:52:47 -0400

    modify navbar initial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,48 @@ export default function Navbar() {
     <nav className="fixed top-0 w-full z-50 bg-black/30 backdrop-blur-md">
       <div className="max-w-7xl mx-auto px-8 h-16 flex items-center justify-between">
 
-        <h1 className="font-bold text-xl">
-          LR
-        </h1>
+        <div className="relative">
+          <div
+            className="
+            absolute
+            -inset-1
+            rounded-full
+            bg-gradient-to-r
+            from-blue-400
+            to-cyan-400
+            blur-md
+            opacity-70
+            "
+          />
+
+          <div
+            className="
+            relative
+            w-12
+            h-12
+            rounded-full
+            flex
+            items-center
+            justify-center
+            font-bold
+            text-white
+
+            bg-[#0b0d12]
+
+            border-2
+            border-white
+            "
+          >
+            LR
+          </div>
+        </div>
 
         <div className="flex gap-8">
           <a href="#projects">Projects</a>
           <a href="#skills">Skills</a>
           <a href="#journey">Journey</a>
-          <a href="#contact">Contact</a>
           <a href="#certifications">Certifications</a>
+          <a href="#contact">Contact</a>
         </div>
 
       </div>
